6a9a5f44d2e7540e69963ef7b35bcd54130763cd
laalex <laalex512@gmail.com>
2022-06-08 16:51:39 +0300

    Add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая на вход
+принимает два числа и выдаёт, какое число большее, а
+какое меньшее.*/
+
+Console.WriteLine("Insert a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine("a ("+a+") bigger then b");
+}
+else if (a<b)
+{
+    Console.WriteLine("b ("+b+") bigger then a");
+}
+else Console.WriteLine("the numbers a and b are equal");
