babd3a223dba9cf43d1fa550c126b38a54d7a542
laalex <laalex512@gmail.com>
2022-06-08 18:49:41 +0300

    Add Additional Task 2

---
+++ AdditionalTask2/Program.cs
@@ -0,0 +1,15 @@
+﻿/*2. Есть программа с бесконечным циклом.
+ Когда пользователь вводит exit программа заканчивается*/
+
+ string stopWord = "exit";
+ string insertMessage;
+ while(true)
+ {
+     Console.WriteLine("Insert anything you want, except '"+stopWord+"':");
+     insertMessage = Console.ReadLine();
+     if (insertMessage==stopWord) 
+     {
+         Console.WriteLine("Bye!");
+         break;
+     }
+ }
